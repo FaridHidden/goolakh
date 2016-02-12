dcf1629cac5c7f2e215574c3d0f7901c3c0383fb
FaridHidden <as793@ymail.com>
2016-02-12 15:20:54 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,21 @@
+do
+
+function run(msg, matches)
+  return 'Goolakh Telegram Bot v1.0'.. [[ 
+  
+  Antispam Bot: @GoolakhBOT
+  Sudo (Admin): Farid
+  
+ ]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
